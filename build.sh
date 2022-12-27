79ad00ba0b38f7cc1888df4487ce31ce34070139
Abhi Sivaprasad <abhi@live.com>
2022-12-27 13:22:23 -0800

    prod deployment kills previous processes; removed build stage

--- build.sh
+++
@@ -1,5 +0,0 @@
-cd server/
-npm install
-
-cd ../client/
-npm install
--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
+# kill existing server and client processes by the ports they listen to
+fuser -k 4001/tcp
+fuser -k 3000/tcp
+
 cd ~/server
+sudo npm install
 sudo npm run start-prod &
 
-cd ../client
+cd ~/client
+sudo npm install
 sudo npm run start-prod &
