ec633ce8b27eec581f31e92a3bb297a62b8e6ab4
Abhi Sivaprasad <abhi@live.com>
2022-12-28 12:48:07 -0800

    wildcard routing

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,10 +9,14 @@ export { app };
 import { router } from './router/router';
 
 // configure middleware
-if (process.env.NODE_ENV !== 'development') {
-    app.use('/', express.static('../client/build'));
-}
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', router);
+
+if (process.env.NODE_ENV !== 'development') {
+    app.use('/', express.static('../client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile('../client/build/index.html');
+    });
+}
