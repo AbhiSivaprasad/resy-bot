b2806d2d047dabd1d003146d54ad04288c588c4d
Abhi Sivaprasad <abhi@live.com>
2022-12-28 13:28:53 -0800

    change prod dir

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
-# kill existing server and client processes by the ports they listen to
+# kill existing server process by the port it listens to
 fuser -k 4001/tcp
-fuser -k 80/tcp
 
-cd /home/ubuntu/resy-bot/client
+cd /home/ubuntu/app/client
 sudo npm install
 npm run build
 
-cd /home/ubuntu/resy-bot/server
+cd /home/ubuntu/app/server
 sudo npm install
 nohup sudo npm run start-prod &
